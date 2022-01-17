08d242fe8730765fdcc205ef24d59cd3e8e4df02
FernandoGnadt <gnadtfernando45@gmail.com>
2022-01-17 12:45:18 -0300

    Melhoria para caso numero negativo

--- teste1.dart
+++ teste1.dart
@@ -8,29 +8,32 @@ main() {
   int somaz = 0;
   int res;
 
+  print('\n-------- Soma de Divisores --------\n');
+
   //Intrução para receber numero do usuário
-  print('Digite um numero inteiro positivo');
+  print('Digite um numero inteiro positivo:');
   inf = int.parse(stdin.readLineSync()!);
 
-  //regra para receber numeros divisiveis de 3
-  if (inf >= 0) {
-    for (int i = 1; i < inf; i++) {
-      if (i % x == 0) {
-        somax += i;
-      }
-    }
-
-    //regra para receber numeros divisiveis de 5
-    for (int i = 1; i < inf; i++) {
-      if (i % z == 0) {
-        somaz += i;
-      }
-    }
-    //expressão para realizar a soma dos divisiveis
-    res = somax + somaz;
-
-    print("A soma dos multiplos de 3 e 5 abaixo de $inf, é: $res");
-  } else {
-    print('Número digitado invalido');
+  while (inf < 0) {
+    print('\nNúmero digitado invalido.');
+    print('Por favor informe um número positivo:');
+    inf = int.parse(stdin.readLineSync()!);
   }
+  //regra para receber numeros divisiveis de 3
+  for (int i = 1; i < inf; i++) {
+    if (i % x == 0) {
+      somax += i;
+    }
+  }
+
+  //regra para receber numeros divisiveis de 5
+  for (int i = 1; i < inf; i++) {
+    if (i % z == 0) {
+      somaz += i;
+    }
+  }
+  //expressão para realizar a soma dos divisiveis
+  res = somax + somaz;
+
+  print("\nA soma dos multiplos de 3 e 5 abaixo de $inf, é: $res\n");
 }
